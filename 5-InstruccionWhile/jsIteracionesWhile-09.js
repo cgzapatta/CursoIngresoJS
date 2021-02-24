3b0de54609c449c97a0087d8ac241c05923b7068
German <cgzapatta@gmail.com>
2021-02-24 05:25:24 -0300

    cambios

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -1,21 +1,56 @@
-/*
-Al presionar el botón pedir  números  hasta que el usuario quiera,
-mostrar el número máximo y el número mínimo ingresado.*/
+//Zapatta, Carlos German.
+
 function mostrar()
-{	// declarar variables
-	var banderaDelPrimero;
+{
 	var numeroIngresado;
-	var numeroMaximo;
-	var numeroMinimo;
-	var respuesta;
-	//iniciar variables
-	banderaDelPrimero="es el primero";
-	respuesta='si';
-	while(respuesta=="si")
+	var parMenor;
+	var negativoMayor;
+	var contador;
+	var primerPar;
+	var primerNegativo;
+	var faltan; 
+	
+	contador=0;
+	faltan=10 //contador de numeros que faltan por ingresar, info para el usuario.
+	primerPar=1; //bandera de primer par ingresado.
+	primerNegativo=1; //bandera de primer negativo ingresado.
+	parMenor="No se ingresaron pares" //valor en caso de no haber pares.
+	negativoMayor="No se ingresaron negativos" //valor en caso de no haber negativos.
+	
+	while(contador<10)
 	{
+		numeroIngresado=prompt("Ingrese un numero"+"\nFaltan "+faltan+" numeros por ingresar");
+		numeroIngresado=parseInt(numeroIngresado);
+
+		while(numeroIngresado==0)
+		{
+			numeroIngresado=prompt("El cero no es valido"+"\nFaltan "+faltan+" numeros por ingresar");
+			numeroIngresado=parseInt(numeroIngresado);
+		}
+		if(primerPar==1 && numeroIngresado%2==0)
+		{
+			parMenor=numeroIngresado;
+			primerPar=0;
+		}
+		if(primerPar==0 && numeroIngresado%2==0 && numeroIngresado<parMenor)
+		{
+			parMenor=numeroIngresado;
+		}
+		if(primerNegativo==1 && numeroIngresado<0)
+		{
+			negativoMayor=numeroIngresado;
+			primerNegativo=0;
+		}
+		if(primerNegativo==0 && numeroIngresado<0 && numeroIngresado>negativoMayor)
+		{
+			negativoMayor=numeroIngresado;
+		}
 		
-		respuesta=prompt("desea continuar?");
+		contador++;
+		faltan=faltan-1;
 	}
-	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimmo.value=numeroMinimo;
-}//FIN DE LA FUNCIÓN
+	txtIdMaximo.value=parMenor;
+	txtIdMinimo.value=negativoMayor;
+}
+// Realizado sobre ejercicio 9.
+//Zapatta, Carlos German.
